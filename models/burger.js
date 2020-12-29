e4bd20ef4a656f644976aef7f622f2df70406874
Greg Stead <gregstead@gmail.com>
2020-12-29 12:48:53 -0600

    Added seeds and started Burger class

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,18 @@
 // Import the ORM
 const orm = require("../config/orm");
 
-class Burger {}
+class Burger {
+  constructor(name) {
+    this.id, (this.name = name);
+  }
+
+  all(cb) {
+    orm.selectAll("burgers", (res) => {
+      cb(res);
+    });
+  }
+}
+
+const burger = new Burger("ham");
+
+burger.all();

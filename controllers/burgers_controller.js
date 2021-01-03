75bd66bdda2538b2f512982179c9896071fd3782
Greg Stead <gregstead@gmail.com>
2021-01-03 11:42:42 -0600

    View rendering, css working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const Burger = require("../models/burger");
 
-// import the model
-const burger = require("../models/burger");
-
 //Create all our routes and set up logic in those routes where required
 router.get("/", (req, res) => {
-  console.log("/GET");
+  Burger.all((data) => {
+    const hbsObject = { burgers: data, layout: "index" };
+    console.log("data :>> ", data);
+    res.render("main", hbsObject);
+  });
 });
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,27 @@
 // Import the ORM
 const orm = require("../config/orm");
 
-class Burger {
+const Burger = {
   constructor(name) {
     this.id, (this.name = name);
-  }
+  },
 
   all(cb) {
     orm.selectAll("burgers", (res) => {
       cb(res);
     });
-  }
+  },
   add(columns, values, cb) {
     orm.insertOne("burgers", columns, values, (err, res) => {
       if (err) throw err;
       cb(res);
     });
-  }
+  },
   update(colVal, condition, cb) {
     orm.updateOne("burgers", colVal, condition, (res) => {
       cb(res);
     });
-  }
-}
+  },
+};
 
 module.exports = Burger;
